a4f04a97b6bc7a33cbbf5a21cf878c57ef76dd04
Nando <24811313+fzavalia@users.noreply.github.com>
2023-12-14 11:52:49 -0300

    feat: Simple signatur page for signature requests

--- src/components/Pages/RequestPage/RequestPage.tsx
+++ src/components/Pages/RequestPage/RequestPage.tsx
@@ -1,29 +1,44 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { ethers } from 'ethers'
+import { useNavigate, useParams } from 'react-router-dom'
 import { Socket, io } from 'socket.io-client'
 import { connection } from 'decentraland-connect'
 import { config } from '../../../modules/config'
+import { SignaturePage } from './SignaturePage/SignaturePage'
 
 export const RequestPage = () => {
   const { requestId } = useParams()
-
+  const navigate = useNavigate()
   const socketRef = useRef<Socket>()
-
+  // TODO: Show error somewehere.
+  const [error, setError] = useState<string | null>(null)
   const [request, setRequest] = useState<any | null>(null)
 
-  const reconnect = useCallback(async () => {
+  const initialize = useCallback(async () => {
     try {
+      // Try to restablish connection with the wallet.
       await connection.tryPreviousConnection()
-      
+    } catch (e) {
+      // If it fails it is because there is no connection and the user needs to login.
+      // The user should login and then be redirected back to this page to continue the transaction.
+      navigate(`/login?redirectTo=/requests/${requestId}`)
+      return
+    }
+
+    try {
       const authServerUrl = config.get('AUTH_SERVER_URL')
+
+      if (!authServerUrl) {
+        throw new Error('Missing AUTH_SERVER_URL')
+      }
+
+      // Connects the browser to the auth server to enable messaging.
       const socket = io(authServerUrl, {})
 
       socket.on('connect', () => {
-        console.log('socket connected')
-
+        // Store a reference to the socket once it is connected so it can be used elsewhere.
         socketRef.current = socket
 
+        // Once connected, recover the request data sent by the client from the auth server.
         socket.emit('message', {
           type: 'recover',
           payload: {
@@ -33,57 +48,48 @@ export const RequestPage = () => {
       })
 
       socket.on('message', (message: any) => {
+        // When the request data is retrieved, store it in the state.
+        // Now the UI should be ready to display what the client intends to do.
         if (message.payload.requestId === requestId) {
-          setRequest(message)
+          if (message.payload.error) {
+            console.error(message.payload.error)
+            setError(message.payload.error)
+          } else {
+            setRequest(message)
+          }
         }
       })
-
-      socket.on('disconnect', () => {
-        console.log('socket disconnected')
-      })
     } catch (e) {
-      console.log('not connected')
+      console.error(e as Error)
+      setError('Cannot connect to the auth server')
     }
   }, [])
 
   useEffect(() => {
-    reconnect()
+    initialize()
 
     return () => {
+      // Close the socket connection when the component is unmounted.
       socketRef.current?.close()
     }
   }, [])
 
-  return (
-    <div>
-      <h1>Request Page</h1>
-      {request ? (
-        <div>
-          <h2>Request Data</h2>
-          <pre>
-            <code>{JSON.stringify(request, null, 2)}</code>
-          </pre>
-          <button
-            onClick={async () => {
-              const provider = await connection.getProvider()
-              const browserProvider = new ethers.BrowserProvider(provider)
-              const signer = await browserProvider.getSigner()
-              const signature = await signer.signMessage(request.payload.data)
+  if (error) {
+    return (
+      <div style={{ margin: '1rem' }}>
+        Failed to fetch the client request from the auth server: <b>${error}</b>
+      </div>
+    )
+  }
 
-              socketRef.current?.emit('message', {
-                type: 'submit-signature',
-                payload: {
-                  requestId,
-                  signer: signer.address,
-                  signature
-                }
-              })
-            }}
-          >
-            Sign
-          </button>
-        </div>
-      ) : null}
-    </div>
-  )
+  if (!request) {
+    return <div style={{ margin: '1rem' }}>Fetching client request from the auth server...</div>
+  }
+
+  switch (request.payload.type) {
+    case 'signature':
+      return <SignaturePage request={request} socketRef={socketRef} />
+    default:
+      return <div style={{ margin: '1rem' }}>Invalid request type: "{request.payload.type}"</div>
+  }
 }
--- src/hooks/redirection.ts
+++ src/hooks/redirection.ts
@@ -6,12 +6,26 @@ export const useAfterLoginRedirection = () => {
   const location = useLocation()
   const search = new URLSearchParams(location.search)
   const redirectToSearchParam = search.get('redirectTo')
-  if (redirectToSearchParam === null) return undefined
+
+  if (redirectToSearchParam === null) {
+    return undefined
+  }
+
   try {
-    const redirectToURL = new URL(redirectToSearchParam)
-    if (isDecentralandDomain.test(redirectToURL.hostname)) {
-      return redirectToURL.href
+    let redirectToURL: URL
+
+    if (redirectToSearchParam.startsWith('/')) {
+      redirectToURL = new URL(redirectToSearchParam, window.location.origin)
+    } else {
+      redirectToURL = new URL(redirectToSearchParam)
+
+      // For complete urls, only allow redirections to decentraland domains.
+      if (!isDecentralandDomain.test(redirectToURL.hostname)) {
+        return undefined
+      }
     }
+
+    return redirectToURL.href
   } catch (error) {
     console.error("Can't parse redirectTo URL")
     return undefined
