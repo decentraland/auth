a5cedbf4ccafc3adcc67f4c8352f51f9fd398b59
Nando <24811313+fzavalia@users.noreply.github.com>
2023-12-26 14:28:10 -0300

    feat: WIP request page

--- src/components/Pages/RequestPage/RequestPage.tsx
+++ src/components/Pages/RequestPage/RequestPage.tsx
@@ -4,6 +4,9 @@ import { ethers } from 'ethers'
 import { io } from 'socket.io-client'
 import { connection } from 'decentraland-connect'
 import { config } from '../../../modules/config'
+import styles from './RequestPage.module.css'
+import { Button } from 'decentraland-ui/dist/components/Button/Button'
+import { Loader } from 'decentraland-ui/dist/components/Loader/Loader'
 
 export const RequestPage = () => {
   const params = useParams()
@@ -11,53 +14,84 @@ export const RequestPage = () => {
   const { request, error: recoverError } = useRecoverRequestFromAuthServer(requestId)
 
   if (recoverError) {
-    return <div style={{ margin: '1rem' }}>Could not recover request...</div>
+    return (
+      <main className={styles.main}>
+        <div className={styles.left}>
+          <div className={styles.errorLogo}></div>
+          <div className={styles.errorTitle}>There was an error obtaining your request...</div>
+          <div className={styles.errorSubtitle}>Close this window and try again.</div>
+          <div className={styles.errorValue}>{recoverError}</div>
+        </div>
+      </main>
+    )
   }
 
   if (!request) {
-    return <div style={{ margin: '1rem' }}>Loading...</div>
+    return (
+      <main className={styles.main}>
+        <div className={styles.left}>
+          <Loader active size="huge" />
+        </div>
+      </main>
+    )
   }
+
+  console.log(request)
 
   if (request.method === 'dcl_personal_sign') {
     return (
-      <div style={{ margin: '1rem' }}>
-        <h1>{request.method}</h1>
-        <pre>{request.params[0]}</pre>
-        <div>
-          Code: <b>{request.code}</b> (This code should be visible on the desktop client as well)
+      <main className={styles.main}>
+        <div className={styles.left}>
+          <div className={styles.logo}></div>
+          <div className={styles.title}>Verify Sign In</div>
+          <div className={styles.description}>Do you see the same verification number on your desktop app?</div>
+          <div className={styles.code}>{request.code}</div>
+          <div className={styles.buttons}>
+            <Button className={styles.noButton}>
+              <div className={styles.noLogo}></div> No
+            </Button>
+            <Button
+              className={styles.yesButton}
+              onClick={async () => {
+                const provider = await connection.getProvider()
+                const browserProvider = new ethers.BrowserProvider(provider)
+                const signer = await browserProvider.getSigner()
+                const signature = await signer.signMessage(request.params[0])
+                await authServerFetch('outcome', { requestId, sender: await signer.getAddress(), result: signature })
+              }}
+            >
+              <div className={styles.yesLogo}></div> Yes, they are the same
+            </Button>
+          </div>
         </div>
-        <button
-          style={{ marginTop: '1rem' }}
-          onClick={async () => {
-            const provider = await connection.getProvider()
-            const browserProvider = new ethers.BrowserProvider(provider)
-            const signer = await browserProvider.getSigner()
-            const signature = await signer.signMessage(request.params[0])
-            await authServerFetch('outcome', { requestId, sender: await signer.getAddress(), result: signature })
-          }}
-        >
-          Sign Ephemeral Message
-        </button>
-      </div>
+      </main>
     )
   } else {
     return (
-      <div style={{ margin: '1rem' }}>
-        <h1>{request.method}</h1>
-        <pre>{JSON.stringify(request.params)}</pre>
-        <button
-          style={{ marginTop: '1rem' }}
-          onClick={async () => {
-            const provider = await connection.getProvider()
-            const browserProvider = new ethers.BrowserProvider(provider)
-            const signer = await browserProvider.getSigner()
-            const result = await browserProvider.send(request.method, request.params)
-            await authServerFetch('outcome', { requestId, sender: await signer.getAddress(), result })
-          }}
-        >
-          Execute
-        </button>
-      </div>
+      <main className={styles.main}>
+        <div className={styles.left}>
+          <div className={styles.logo}></div>
+          <div className={styles.title}>Remote Wallet Interaction</div>
+          <div className={styles.description}>The desktop app is trying to interact with your wallet.</div>
+          <div className={styles.buttons}>
+            <Button className={styles.noButton}>
+              <div className={styles.noLogo}></div>Deny
+            </Button>
+            <Button
+              className={styles.yesButton}
+              onClick={async () => {
+                const provider = await connection.getProvider()
+                const browserProvider = new ethers.BrowserProvider(provider)
+                const signer = await browserProvider.getSigner()
+                const result = await browserProvider.send(request.method, request.params)
+                await authServerFetch('outcome', { requestId, sender: await signer.getAddress(), result })
+              }}
+            >
+              <div className={styles.yesLogo}></div>Allow
+            </Button>
+          </div>
+        </div>
+      </main>
     )
   }
 }
