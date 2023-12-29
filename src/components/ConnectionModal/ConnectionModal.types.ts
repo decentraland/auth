8202c3c06727a082ddf507c33071405481527a2c
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2023-12-29 11:19:54 -0300

    fix: add loading states and mobile support (#35)

--- src/components/ConnectionModal/ConnectionModal.types.ts
+++ src/components/ConnectionModal/ConnectionModal.types.ts
@@ -1,13 +1,17 @@
+import { ProviderType } from '@dcl/schemas'
+
 export enum ConnectionModalState {
   CONNECTING_WALLET = 'CONNECTING_WALLET',
   WAITING_FOR_SIGNATURE = 'WAITING_FOR_SIGNATURE',
   ERROR = 'ERROR',
-  LOADING_MAGIC = 'LOADING_MAGIC'
+  LOADING_MAGIC = 'LOADING_MAGIC',
+  VALIDATING_SIGN_IN = 'VALIDATING_SIGN_IN'
 }
 
 export type ConnectionModalProps = {
   open: boolean
   state: ConnectionModalState
-  onClose: () => unknown
+  providerType: ProviderType | null
+  onClose?: () => unknown
   onTryAgain: () => unknown
 }
--- src/components/ConnectionModal/utils.ts
+++ src/components/ConnectionModal/utils.ts
@@ -1,15 +1,19 @@
+import { ProviderType } from '@dcl/schemas'
 import { ConnectionModalState } from './ConnectionModal.types'
 
-export function getConnectionMessage(connectionState: ConnectionModalState) {
+export function getConnectionMessage(connectionState: ConnectionModalState, providerType: ProviderType | null) {
   switch (connectionState) {
     case ConnectionModalState.ERROR: {
-      return 'You rejected the request in your <b>Wallet</b>. To continue, please try again.'
+      return 'You did not confirm this action in your digital wallet extension. To continue, please try again.'
     }
-    case ConnectionModalState.CONNECTING_WALLET: {
-      return 'To move forward, confirm the connect action in your <b>Wallet</b>.'
-    }
+    case ConnectionModalState.CONNECTING_WALLET:
     case ConnectionModalState.WAITING_FOR_SIGNATURE: {
-      return 'To move forward, confirm the signature action in your <b>Wallet</b>.'
+      return providerType === ProviderType.MAGIC
+        ? 'Almost done! Confirm your request to login Decentraland'
+        : 'Confirm in your digital wallet extension to continue.'
+    }
+    case ConnectionModalState.VALIDATING_SIGN_IN: {
+      return "Just a moment, we're verifying your login credentials..."
     }
     case ConnectionModalState.LOADING_MAGIC: {
       return 'Redirecting...'
