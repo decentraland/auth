832692026e832c4232c7b5a2944965e59853eaba
Nando <24811313+fzavalia@users.noreply.github.com>
2024-01-25 15:24:14 -0300

    fix: Fix config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ command, mode }) => {
   const envVariables = loadEnv(mode, process.cwd())
   return {
     plugins: [
@@ -17,6 +17,17 @@ export default defineConfig(({ mode }) => {
         // eslint-disable-next-line @typescript-eslint/naming-convention
         VITE_REACT_APP_DCL_DEFAULT_ENV: envVariables.VITE_REACT_APP_DCL_DEFAULT_ENV
       }
-    }
+    },
+    ...(command === 'build'
+      ? {
+          base: envVariables.VITE_BASE_URL,
+          build: {
+            commonjsOptions: {
+              transformMixedEsModules: true
+            },
+            sourcemap: true
+          }
+        }
+      : undefined)
   }
 })
