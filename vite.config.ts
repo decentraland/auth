48af41863934f9f10aa30c506f708722ff1fa57b
Nando <24811313+fzavalia@users.noreply.github.com>
2024-01-25 14:20:56 -0300

    fix: Remove other polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 import react from '@vitejs/plugin-react-swc'
-import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
 import { defineConfig, loadEnv } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const envVariables = loadEnv(mode, process.cwd())
   return {
     plugins: [
@@ -20,36 +17,6 @@ export default defineConfig(({ command, mode }) => {
         // eslint-disable-next-line @typescript-eslint/naming-convention
         VITE_REACT_APP_DCL_DEFAULT_ENV: envVariables.VITE_REACT_APP_DCL_DEFAULT_ENV
       }
-    },
-    ...(command === 'build'
-      ? {
-          base: envVariables.VITE_BASE_URL,
-          optimizeDeps: {
-            esbuildOptions: {
-              // Node.js global to browser globalThis
-              define: {
-                global: 'globalThis'
-              },
-              // Enable esbuild polyfill plugins
-              plugins: [
-                NodeGlobalsPolyfillPlugin({
-                  buffer: true,
-                  process: true
-                }),
-                NodeModulesPolyfillPlugin()
-              ]
-            }
-          },
-          build: {
-            commonjsOptions: {
-              transformMixedEsModules: true
-            },
-            rollupOptions: {
-              plugins: [rollupNodePolyFill()]
-            },
-            sourcemap: true
-          }
-        }
-      : undefined)
+    }
   }
 })
