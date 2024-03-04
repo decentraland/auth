806f6ef3d9446b59270597a446250785bff0afc8
Lean Mendoza <leandro@decentraland.org>
2024-03-04 12:22:24 -0300

    feat: add config layout by query params (#102)
    * feat: add config layout by query params
    
    * typo
    
    * apply review
    
    * remove facebook
    
    * Delete src/assets/images/facebook.svg
    
    Signed-off-by: Lean Mendoza <leandro@decentraland.org>
    
    * fix vertical center
    
    ---------
    
    Signed-off-by: Lean Mendoza <leandro@decentraland.org>

---
+++ src/hooks/targetConfig.ts
@@ -0,0 +1,33 @@
+import { useLocation } from 'react-router-dom'
+
+type TargetConfigId = 'default' | 'alternative'
+type TargetConfig = {
+  skipVerifyCode: boolean
+  skipSetup: boolean
+  showWearablePreview: boolean
+  explorerText: string
+}
+
+const targetConfigRecord: Record<TargetConfigId, TargetConfig> = {
+  default: {
+    skipVerifyCode: false,
+    skipSetup: false,
+    showWearablePreview: true,
+    explorerText: 'Desktop App'
+  },
+  alternative: {
+    skipVerifyCode: true,
+    skipSetup: true,
+    showWearablePreview: false,
+    explorerText: 'Explorer'
+  }
+}
+
+export const useTargetConfig = (): [TargetConfig, TargetConfigId] => {
+  const location = useLocation()
+  const search = new URLSearchParams(location.search)
+  const targetConfigIdSearchParam = search.get('targetConfigId') || 'default'
+  const targetConfigId =
+    targetConfigIdSearchParam && targetConfigIdSearchParam in targetConfigRecord ? (targetConfigIdSearchParam as TargetConfigId) : 'default'
+  return [targetConfigRecord[targetConfigId], targetConfigId]
+}
