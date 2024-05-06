956871c136b3942eb9290a70f956664a22f6a8a1
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2024-05-06 17:15:44 -0300

    fix: Deploy updated profiles (#114)
    * fix: Test alpha eyes
    
    * fix: Remove skipping flag
    
    * fix: Remove FFs
    
    * fix: Use testing peer
    
    * fix: Use peer testing URL
    
    * fix: Remove default legacy profiles deployment logic

--- src/components/Pages/SetupPage/utils.ts
+++ src/components/Pages/SetupPage/utils.ts
@@ -1,10 +1,8 @@
 import { createFetchComponent } from '@well-known-components/fetch-component'
 import { DeploymentBuilder, createContentClient } from 'dcl-catalyst-client'
 import { AuthIdentity, Authenticator } from '@dcl/crypto'
-import { hashV1 } from '@dcl/hashing'
 import { EntityType } from '@dcl/schemas'
 import { config } from '../../../modules/config'
-import { resizeImage } from './resizeImage'
 
 export async function subscribeToNewsletter(email: string) {
   const url = config.get('BUILDER_SERVER_URL')
@@ -44,12 +42,8 @@ export async function deployProfileFromDefault({
   const defaultEntities = await client.fetchEntitiesByPointers([defaultProfile])
   const defaultEntity = defaultEntities[0]
 
-  // Check if the retrieved entity belongs to a legacy profile.
-  const isLegacy = defaultEntity.id.startsWith('Qm')
-
   // Download the content of the default profile and create the files map to be used for deploying the new profile.
   const bodyFile = 'body.png'
-  const faceFile = 'face.png'
   const face256File = 'face256.png'
 
   const contentHashesByFile = defaultEntity.content.reduce(
@@ -57,17 +51,14 @@ export async function deployProfileFromDefault({
     {} as Record<string, string>
   )
 
-  const bodyBuffer = await client.downloadContent(contentHashesByFile[bodyFile])
-  const faceBuffer = !isLegacy
-    ? await client.downloadContent(contentHashesByFile[face256File])
-    : // Legacy profiles have a face.png file that needs to be resized to 256x256.
-      await resizeImage(await client.downloadContent(contentHashesByFile[faceFile]))
+  const [bodyBuffer, faceBuffer] = await Promise.all([
+    client.downloadContent(contentHashesByFile[bodyFile]),
+    client.downloadContent(contentHashesByFile[face256File])
+  ])
 
   const files = new Map<string, Uint8Array>()
 
   files.set(bodyFile, bodyBuffer)
-  // We are going to upload a new profile that needs to have a face256 file.
-  // We can use the face file from the legacy profile for it.
   files.set(face256File, faceBuffer)
 
   // Both org and zone content server profiles have legacy ids for wearables and body shapes.
@@ -75,7 +66,7 @@ export async function deployProfileFromDefault({
   const mapLegacyIdToUrn = (urn: string) => urn.replace('dcl://base-avatars/', 'urn:decentraland:off-chain:base-avatars:')
 
   // Override the default avatar with the provided name and connected account address.
-  const avatar = defaultEntity.metadata?.avatars?.[0]
+  const avatar = defaultEntity.metadata.avatars?.[0]
 
   avatar.name = deploymentProfileName
   avatar.ethAddress = connectedAccount
@@ -87,11 +78,6 @@ export async function deployProfileFromDefault({
   avatar.avatar.bodyShape = mapLegacyIdToUrn(defaultEntity.metadata.avatars[0].avatar.bodyShape)
   avatar.avatar.wearables = defaultEntity.metadata.avatars[0].avatar.wearables.map(mapLegacyIdToUrn)
   avatar.avatar.emotes = []
-
-  if (isLegacy) {
-    const [body, face256] = await Promise.all([hashV1(bodyBuffer), hashV1(faceBuffer)])
-    avatar.avatar.snapshots = { body, face256 }
-  }
 
   // Build the entity for the profile to be deployed.
   const deploymentEntity = await DeploymentBuilder.buildEntity({
