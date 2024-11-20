2f5e7410d9979bd53ccc7d5b5922cdb6eccefe4f
Mateo "Kuruk" Miccino <mateomiccino@gmail.com>
2024-11-20 19:49:24 -0300

    add required tests in targetConfig from feedback

--- src/hooks/targetConfig.spec.ts
+++ src/hooks/targetConfig.spec.ts
@@ -36,6 +36,39 @@ describe('useTargetConfig', () => {
     expect(config).toEqual(_targetConfigs.ios)
   })
 
+  it('returns android config when targetConfigId=android', () => {
+    ;(useLocation as jest.Mock).mockReturnValue({ search: '?targetConfigId=android' })
+    ;(isMobile as jest.Mock).mockReturnValue(false)
+
+    const { result } = renderHook(() => useTargetConfig())
+    const [config, targetConfigId] = result.current
+
+    expect(targetConfigId).toBe('android')
+    expect(config).toEqual(_targetConfigs.android)
+  })
+
+  it('returns androidSocial config when targetConfigId=androidSocial', () => {
+    ;(useLocation as jest.Mock).mockReturnValue({ search: '?targetConfigId=androidSocial' })
+    ;(isMobile as jest.Mock).mockReturnValue(false)
+
+    const { result } = renderHook(() => useTargetConfig())
+    const [config, targetConfigId] = result.current
+
+    expect(targetConfigId).toBe('androidSocial')
+    expect(config).toEqual(_targetConfigs.androidSocial)
+  })
+
+  it('returns androidWeb3 config when targetConfigId=androidWeb3', () => {
+    ;(useLocation as jest.Mock).mockReturnValue({ search: '?targetConfigId=androidWeb3' })
+    ;(isMobile as jest.Mock).mockReturnValue(false)
+
+    const { result } = renderHook(() => useTargetConfig())
+    const [config, targetConfigId] = result.current
+
+    expect(targetConfigId).toBe('androidWeb3')
+    expect(config).toEqual(_targetConfigs.androidWeb3)
+  })
+
   it('returns the mobile-adjusted config for default config on mobile', () => {
     ;(useLocation as jest.Mock).mockReturnValue({ search: '?targetConfigId=default' })
     ;(isMobile as jest.Mock).mockReturnValue(true)
