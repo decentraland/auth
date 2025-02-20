6694b678fee08ebbac6dc6e148949fbfb141b784
LautaroPetaccio <lausuper@gmail.com>
2025-02-20 13:30:58 -0300

    feat: Add more analytics to the auth site

--- src/modules/analytics/types.ts
+++ src/modules/analytics/types.ts
@@ -4,7 +4,11 @@ export enum TrackingEvents {
   LOGIN_ERROR = 'Login Error',
   CLICK = 'Click Button',
   AVATAR_EDIT_SUCCESS = 'Avatar Edit Success',
-  TERMS_OF_SERVICE_SUCCESS = 'Terms of service success'
+  TERMS_OF_SERVICE_SUCCESS = 'Terms of service success',
+  REQUEST_EXPIRED = 'Expired request',
+  REQUEST_LOADING_ERROR = 'Error loading request',
+  WALLET_INTERACTION_REQUEST_TYPE = 'Wallet interaction request type',
+  VERIFY_SIGN_IN_REQUEST_TYPE = 'Verify sign in request type'
 }
 
 export enum ClickEvents {
