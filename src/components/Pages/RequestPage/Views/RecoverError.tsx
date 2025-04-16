ed84cb0f9ba7ab7406f5754b5c524bfe5b1f165d
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2025-04-16 17:21:07 -0300

    fix: HTTP Client errors (#155)
    * fix: HTTP Client errors
    
    * fix: Tests

--- src/components/Pages/RequestPage/Views/RecoverError.tsx
+++ src/components/Pages/RequestPage/Views/RecoverError.tsx
@@ -8,7 +8,7 @@ export const RecoverError = ({ error }: { error: React.ReactNode }) => (
     <div className={styles.title}>There was an error recovering the request...</div>
     <div className={styles.description}>
       The request is not available anymore. Feel free to create a new one and try again. If the expiration time is still running in the
-      Explorer app, check your computer's time to see if it's set correctly
+      Explorer app, check your computer's time to see if it's set correctly.
     </div>
     <CloseWindow />
     <div className={styles.errorMessage}>{error}</div>
