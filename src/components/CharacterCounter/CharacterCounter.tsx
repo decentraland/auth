ad245124c8f01eef6a98c10be61e617aa90fa5b2
Braian Mellor <braianj@gmail.com>
2026-01-16 17:58:30 -0300

    fix: type lint

--- src/components/CharacterCounter/CharacterCounter.tsx
+++ src/components/CharacterCounter/CharacterCounter.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import { CharacterCounter, CharacterCounterText, WarningIcon, ErrorText } from './CharacterCounter.styled'
 import { CharacterCounterProps } from './CharacterCounter.types'
 
