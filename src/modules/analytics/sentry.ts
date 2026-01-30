bf37ee0e18193e6259f77342fcb052d23fc75cf2
Braian Mellor <braianj@gmail.com>
2026-01-29 22:21:53 -0300

    feat: disable Sentry event reporting on mobile devices

--- src/modules/analytics/sentry.ts
+++ src/modules/analytics/sentry.ts
@@ -1,6 +1,10 @@
 import { init, browserTracingIntegration, replayIntegration } from '@sentry/react'
 import { Env } from '@dcl/ui-env/dist/env'
+import { isMobile } from '../../components/Pages/LoginPage/utils'
 import { config } from '../config'
+
+const isMobileDevice = isMobile()
+const isSentryEnabled = !config.is(Env.DEVELOPMENT) && !isMobileDevice
 
 init({
   environment: config.get('ENVIRONMENT'),
@@ -8,18 +12,28 @@ init({
   dsn: config.get('SENTRY_DSN'),
   integrations: [browserTracingIntegration(), replayIntegration()],
   // Performance Monitoring
-  tracesSampleRate: 0.001,
+  tracesSampleRate: isSentryEnabled ? 0.001 : 0,
   // Session Replay
-  replaysSessionSampleRate: 0.01,
-  replaysOnErrorSampleRate: 0.01,
-  enabled: !config.is(Env.DEVELOPMENT),
+  replaysSessionSampleRate: isSentryEnabled ? 0.01 : 0,
+  replaysOnErrorSampleRate: isSentryEnabled ? 0.01 : 0,
+  enabled: isSentryEnabled,
   beforeSend(event) {
+    // Filter out events from mobile devices
+    if (isMobileDevice) {
+      return null
+    }
     // Filter out exceptions from GTM and STAG
     if (
       event.exception?.values?.some(exception =>
         exception.stacktrace?.frames?.some(frame => frame.filename?.includes('gtm') || frame.filename?.includes('stag'))
       )
     ) {
+      return null
+    }
+    return event
+  },
+  beforeSendTransaction(event) {
+    if (isMobileDevice) {
       return null
     }
     return event
