952f53d2ed9998a6cbc81194d96c689486c13c91
LautaroPetaccio <lausuper@gmail.com>
2026-02-27 18:41:29 -0300

    feat: Add italian and french

--- src/modules/translations/index.ts
+++ src/modules/translations/index.ts
@@ -1,5 +1,7 @@
 import en from './en.json'
 import es from './es.json'
+import fr from './fr.json'
+import it from './it.json'
 import zh from './zh.json'
 
-export const translations = { en, es, zh }
+export const translations = { en, es, fr, it, zh }
