c9e11d1757d969e3c12e1951a7e80af7eb4b018c
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2026-03-05 16:48:54 -0300

    fix: Email setup with + sign (#323)
    * fix: Email setup with + sign
    
    * fix: Linting

---
+++ src/shared/email.ts
@@ -0,0 +1,5 @@
+// RFC 5322 compliant email regex
+const EMAIL_REGEX =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/
+
+export const isEmailValid = (email: string): boolean => EMAIL_REGEX.test(email)
