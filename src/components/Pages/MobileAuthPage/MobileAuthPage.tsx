756be69d6ad7b4d3198a094d8bd264a2c17a4d7c
LautaroPetaccio <lausuper@gmail.com>
2026-03-06 09:04:54 -0300

    fix: Remove Mobile provider check

--- src/components/Pages/MobileAuthPage/MobileAuthPage.tsx
+++ src/components/Pages/MobileAuthPage/MobileAuthPage.tsx
@@ -9,13 +9,7 @@ import { ConnectionOptionType } from '../../Connection'
 import { ConnectionLayout } from '../../ConnectionModal/ConnectionLayout'
 import { ConnectionLayoutState } from '../../ConnectionModal/ConnectionLayout.type'
 import { FeatureFlagsContext, FeatureFlagsKeys } from '../../FeatureFlagsProvider'
-import {
-  connectToProvider,
-  connectToSocialProvider,
-  fromConnectionOptionToProviderType,
-  isSocialLogin,
-  requiresInjectedProvider
-} from '../LoginPage/utils'
+import { connectToProvider, connectToSocialProvider, fromConnectionOptionToProviderType, isSocialLogin } from '../LoginPage/utils'
 import { getIdentitySignature } from './identityUtils'
 import { MobileAuthSuccess } from './MobileAuthSuccess'
 import { MobileProviderSelection } from './MobileProviderSelection'
@@ -58,10 +52,6 @@ export const MobileAuthPage = () => {
             true // isMobileFlow
           )
         } else {
-          // Web3 wallet flow
-          if (requiresInjectedProvider(selectedConnectionType) && !window.ethereum) {
-            throw new Error('No wallet extension detected. Please install MetaMask or another Ethereum wallet.')
-          }
           setView('connecting')
           setLoadingState(ConnectionLayoutState.CONNECTING_WALLET)
           const connectionData = await connectToProvider(selectedConnectionType)
