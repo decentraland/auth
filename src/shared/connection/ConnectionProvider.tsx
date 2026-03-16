8406b154ceaa8143e01a2fac79d7b3ca43bf65b4
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2026-03-16 16:44:31 -0300

    fix: handlers only for supporting wallets (#353)
    * fix: Add wallet handlers only on those who support it
    
    * fix: Use removeListener

--- src/shared/connection/ConnectionProvider.tsx
+++ src/shared/connection/ConnectionProvider.tsx
@@ -131,12 +131,14 @@ const ConnectionProvider = ({ children }: PropsWithChildren) => {
       setState(prev => ({ ...prev, chainId: parseInt(chainId, 16) }))
     }
 
+    if (typeof provider.on !== 'function') return
+
     provider.on('accountsChanged', handleAccountsChanged)
     provider.on('chainChanged', handleChainChanged)
 
     return () => {
-      provider.off('accountsChanged', handleAccountsChanged)
-      provider.off('chainChanged', handleChainChanged)
+      provider.removeListener('accountsChanged', handleAccountsChanged)
+      provider.removeListener('chainChanged', handleChainChanged)
     }
   }, [state.provider])
 
