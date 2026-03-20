2a4b2c68f9c20001b0e082bcb2e1767ed65a4e79
LautaroPetaccio <lausuper@gmail.com>
2026-03-20 18:09:52 -0300

    fix: Linting

--- src/shared/utils/errorHandler.ts
+++ src/shared/utils/errorHandler.ts
@@ -4,8 +4,7 @@ import { isErrorWithMessage } from '../errors'
 import { trackEvent } from './analytics'
 import { ErrorContext, HandleErrorOptions } from './errorHandler.types'
 
-const shouldSkipReporting = (error: unknown): boolean =>
-  error instanceof Error && 'skipReporting' in error && error.skipReporting === true
+const shouldSkipReporting = (error: unknown): boolean => error instanceof Error && 'skipReporting' in error && error.skipReporting === true
 
 const handleError = (error: unknown, context: string, options?: HandleErrorOptions) => {
   if (shouldSkipReporting(error)) {
