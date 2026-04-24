b797467fc7c9aee4dec49814bca00f20fd532249
Juanma Hidalgo <juanma06@gmail.com>
2026-04-24 14:12:25 +0200

    fix: avoid bare-spinner flash between callback and request page (#384)

---
+++ src/components/VerifyingCredentialsView/VerifyingCredentialsView.styled.ts
@@ -0,0 +1,19 @@
+import { Box, styled } from 'decentraland-ui2'
+
+const Container = styled(Box)({
+  display: 'flex',
+  height: '100vh',
+  width: '100vw',
+  position: 'relative'
+})
+
+const Wrapper = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+  width: '100%'
+})
+
+export { Container, Wrapper }
